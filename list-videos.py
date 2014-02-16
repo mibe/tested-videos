4dbd96af9cfba7c4e8c44ee897b141f09f048484
Michael Bemmerl <mail@mx-server.de>
2014-02-16 01:34:27 +0100

    Initial checkin

---
+++ list-videos.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""Listing video URLs of stories on tested.com.
+
+Copyright: (C) 2014 Michael Bemmerl
+License: MIT License (see LICENSE.txt)
+
+Requirements:
+- Python (well, obviously ;-)
+
+Tested with Python 2.7.6
+"""
+
+
